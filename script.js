bea3a02c08ca25d27b7172ea58c4380846ad4962
quenobie8000 <136032697+quenobie8000@users.noreply.github.com>
2025-05-12 21:07:10 -0400

    ghaerhr

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const button = document.getElementById("button");
 const clickEffect = document.getElementById("click-effect");
 const balance = document.getElementById("balance");
 const reset = document.getElementById("reset");
+const upgradeArea = document.getElementById("upgradeArea");
+const upgrades = document.getElementById("upgrades");
 
 let balanceStorage = localStorage.getItem("balance");
 let buttonValueStorage = localStorage.getItem("buttonValue");
@@ -36,6 +38,25 @@ function resetBal() {
   updateBalance();
 }
 
+function createElement(){
+
+}
+
+function compactButton(){
+  if (window.innerWidth < 815) {
+    upgradeArea.classList.remove("default");
+    upgradeArea.classList.add("compact");
+    upgrades.classList.remove("default");
+    upgrades.classList.add("compact");
+  }
+  else {
+    upgradeArea.classList.remove("compact");
+    upgradeArea.classList.add("default");
+    upgrades.classList.add("default");
+    upgrades.classList.remove("compact");
+  }
+}
+
 onClick(button, () => IDaddTempClass(button, "pressed", 200));
 onClick(button, () => balanceValue += 1);
 onClick(button, () => updateBalance());
@@ -48,4 +69,5 @@ onClick(reset,()=>resetBal());
 
 setInterval(()=> {
   saveGame();
+  compactButton();
 },100);
